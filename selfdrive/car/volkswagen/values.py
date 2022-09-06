d79d192139f6ac9e8ef6e1723a6919fce371adc9
captgao <48182702+captgao@users.noreply.github.com>
2022-09-06 00:21:12 -0400

    Fix IntFlag

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -1,6 +1,6 @@
 from collections import defaultdict, namedtuple
 from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, IntFlag
 from typing import Dict, List, Union
 
 from cereal import car
